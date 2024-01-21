f2e04a08e7b1ac654b9a16e53368a3c0e78f793e
Steve The Animator <103543324+SteveTheAnimator@users.noreply.github.com>
2024-01-21 07:14:21 -0700

    Update PluginInfo.cs

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -1,12 +1,12 @@
-﻿namespace CppPlayerVolume
+namespace CppPlayerMax
 {
     /// <summary>
     /// This class is used to provide information about your mod to BepInEx.
     /// </summary>
     internal class PluginInfo
     {
-        public const string GUID = "com.kylethescientist.gorillatag.cppplayervolume";
-        public const string Name = "CppPlayerVolume";
+        public const string GUID = "com.steve.gorillatag.cppplayermax";
+        public const string Name = "CppPlayerMax";
         public const string Version = "1.0.0";
     }
 }
